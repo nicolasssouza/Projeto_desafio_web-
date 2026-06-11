30e47147b8029fdb036af72ca7aafcc0ec4a82df
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-11 16:35:22 -0300

    Criado os comandos fundamentais de js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,93 @@
 console.log("Test")
+
+console.log("Comando de saída")
+
+//COMENTÁRIO DE LINHA SIMPLES
+
+/* 
+COMENTÁRIO DE MÚLTIPLAS LINHAS,
+OUSEJA, POSSO DESCREVER ALGO
+NESSE TIO DE COMENTÁRIO...
+*/
+
+//CRIANDO VARIÁVEIS
+let num= 50
+var num2= 180
+const num3= 450
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+    let num4= 420
+    var num5= 180
+
+    console.log(num4)
+
+    console.log(num)
+}
+
+console.log(num5)
+
+num = 5
+
+console.log(num)
+
+num2 = 6
+
+console.log(num2)
+
+//num3 = 12
+
+//CONCATENAÇÃO
+console.log("Valor de num" + num)
+console.log('Valor de num3', num3)
+console.log(`Valor de num2 ${num2}`)
+console.log('A soma dos números' + 15 + 8)
+console.log('A soma dos números', 15 + 8)
+console.log(`A soma dos números ${15 + 8}`)
+console.log('A soma dos números' +num + num2)
+console.log('A soma dos números', num2 + 8)
+console.log(`A soma dos números ${num3 - num2}`)
+
+//OPERADORES MATEMÁTICOS
+/*
+   + SOMA
+   - SUBTRAÇÃO
+   = MULTIPLICAÇÃO
+   / DIVISÃO
+   % MÓDULO (RESTO DA DIVISÃO)
+*/
+
+console.log('SOMA DAS VARIÁVEOS', num + num2)
+console.log('SUBTRAÇÃO DAS VARIÁVEIS', num3 - num)
+console.log('MULTIPLICAÇÃO', num5* 2)
+console.log('DIVISÃO DE VALORES', num2 / 5)
+console.log('RESTO DA DIVISÃO', num2 % 2)
+console.log(`RESTO DA DIVISÃO ${num3 % 3}`)
+
+//OPERADORES RELACIONAIS
+/* 
+   == IGUAL
+   > MAIOR
+   < MENOR
+   >= MAIOR IGUAL
+   <= MENOR IGUAL
+   != DIFERENTE
+*/
+
+//OPERADORES LÓGICO
+/*
+  && E
+  || OU
+  */
+ console.log('TESTE DE COMPARAÇÃO', 10 == 5)
+ console.log('TESTE RELACIONAIS', 10 !=5)
+ console.log('TESTE RELACIONAIS', 10 > 5)
+ console.log('TESTE RELACIONAIS', 10 < 5)
+ console.log('TESTE RELACIONAIS', 10 >= 5)
+ console.log('TESTE RELACIONAIS', 10 <= 5)
+ console.log('TESTE LÓGICO', (10 > 5) && (15 < 8))
+ console.log('TESTE LÓGICO', (10 > 5) || (15 < 8))
+

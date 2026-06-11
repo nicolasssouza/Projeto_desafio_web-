a24f8765bd8ef9eee04a947a83b69f41edf2b63a
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-11 16:56:09 -0300

    Adicionado estrtura de idade

--- js/script.js
+++ js/script.js
@@ -91,3 +91,31 @@ console.log(`RESTO DA DIVISÃO ${num3 % 3}`)
  console.log('TESTE LÓGICO', (10 > 5) && (15 < 8))
  console.log('TESTE LÓGICO', (10 > 5) || (15 < 8))
 
+
+ //TESTE LÓGICO
+ let idade = 25
+
+ if (idade >= 18){
+    console.log('Maior de idade')
+ }else{
+    console.log('Menor de idade')
+ }
+ 
+ /**
+  * RENOVAÇÃO DE CNH
+  MENOR QUE 18 - NÃO PERMITIDO
+  18 ATÉ 49 - 10 ANOS
+  50 ATÉ 69 - 5 ANOS
+  70 ACIMA - 3 ANOS
+  */
+
+  if (idade < 18){
+    console.log(`COM A IDADE DE ${idade}, NÃO É PERMITIDO POSSUIR CNH`)
+  }else if(idade < 50){
+    console.log(`COM IDADE DE ${idade}, 10 ANOS PARA RENOVAR`)
+  }else if(idade < 70){
+    console.log(`COM A IDADE de ${idade}, 5 ANOS PARA RENOVAR`)
+  }else{
+    console.log(`COM A IDADE DE ${idade}, 3 ANOS PARA RENOVAR`)
+  }
+

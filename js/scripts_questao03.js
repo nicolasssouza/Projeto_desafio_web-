67368ce18e768f7c3e2e2d8a68cb3373fb9a5415
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-15 14:25:30 -0300

    Modificações no js/scripts_questao03.js

--- js/scripts_questao03.js
+++ js/scripts_questao03.js
@@ -8,14 +8,16 @@ formDados.addEventListener('submit', (evt)=>{
 
     const objFormDados = new FormData(formDados)
 
-    let altura = parseFloat(objFormDados.get('Altura'))
-    let largura = parseFloat(objFormDados.get('Largura'))
+    let distancia = parseFloat(objFormDados.get('Distância'))
+    let consumo = parseFloat(objFormDados.get('Consumo'))
+    let preço = parseFloat(objFormDados.get('Preço'))
      
-    let area = altura * largura
+    let quantidade = distancia / consumo
 
     
 
-    divResultado.innerHTML = `A área a ser pintada é de: ${area.toFixed(2).replace('.',',')}m² , Total de litros para pintar essa área é de ${parseFloat(area / 2).toFixed(2).replace('.',',')}l`
+    divResultado.innerHTML = `A quantidade de combustível necessário é de: ${quantidade.toFixed(2).replace('.',',')}l , O valor total a pagar com combustível para esse
+    deslocamento é de: . ${parseFloat(quantidade * preço).toFixed(2).replace('.',',')}R$`
 
     formDados.reset()
 })
